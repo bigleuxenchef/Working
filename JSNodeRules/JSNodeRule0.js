33c6f22df463d4c4d659fbf208c1405f060faf0d
rumi <bigleuxenchef@gmail.com>
2017-04-08 15:00:59 -0400

    upd

--- JSNodeRules/JSNodeRule0.js
+++ JSNodeRules/JSNodeRule0.js
@@ -1,24 +1,20 @@
 /**
- * 
+ * http://usejsdoc.org/
  */
 
-// 'use strict'
+
+
 
 /*
- * This program is based on the hello-world example from the README.
+ * This program is based on the hello-world example 
  * modify by Rumi
- * Usage: node ./examples/01-hello-world.js
  * 
  * For detailed output: DEBUG=json-rules-engine node
- * ./examples/01-hello-world.js
  */
 
 require('colors')
 require('json-rules-engine')
-// let Engine = require('node_modules/json-rules-engine/dist').Engine
-// let Rule = require('node_modules/json-rules-engine/dist').Rule
-
-// rumi march 22 fix bug in the sample
+require('os')
 let Engine = require('json-rules-engine').Engine
 let Rule = require('json-rules-engine').Rule
 
@@ -53,7 +49,8 @@ let rule = new Rule({
   event: {
     type: 'message',
     params: {
-      data: 'hello-world!',
+      data: '*** greaterThan ***',
+      fact : 'a',
       k : { fact: 'b'}
     }
   },
@@ -61,7 +58,7 @@ let rule = new Rule({
   onSuccess: function (event, almanac)
      {
 	  let  aa = almanac.factValue('a').then(a => {return 9}).then(function (done){return done});
-	  let  bb = almanac.factValue('b').then(a => {return 7});
+	  let  bb = almanac.factValue('b').then(a => {return 7}).then(function (done){return done});
 	  let cc = aa + bb
      // Promise.all(aa,bb,cc).prototype.catch();
 	  console.log('aa ' + aa +'bb '+ bb +'cc ' + cc);
@@ -75,25 +72,76 @@ let rule = new Rule({
   
 })
 
+let rule2 = new Rule({
+	  // define the 'conditions' for when "hello world" should display
+	  conditions: {
+	    all: [{
+	      fact: 'a',
+	      operator: 'lessThanInclusive',
+	      value: { fact: 'b'}
+	    }]
+	  },
+	  // define the 'event' that will fire when the condition evaluates truthy
+	  event: {
+	    type: 'message',
+	    params: {
+	      data: '*** lessThanInclusive ***',
+	      fact : 'a',
+	      k : { fact: 'b'}
+	    }
+	  }
+})
+
+let rule3 = new Rule({
+	  // define the 'conditions' for when "hello world" should display
+	  conditions: {
+	    all: [{
+	      fact: 'a',
+	      operator: 'greaterThanInclusive',
+	      value: { fact: 'b'}
+	    }]
+	  },
+	  // define the 'event' that will fire when the condition evaluates truthy
+	  event: {
+	    type: 'message',
+	    params: {
+	      data: '*** greaterThanInclusive ***',
+	      fact : 'a',
+	      k : { fact: 'b'}
+	    }
+	  }
+})
+
 // add rule to engine
 engine.addRule(rule)
+engine.addRule(rule2)
+engine.addRule(rule3)
+
+
 
 
 // run the engine
 
 
-for (i=0;i<9;i++)
-{
-	
-	let facts = { a: i, b : 6 , c : i}
 
- tmp = engine
-  .run(facts)
+i = 7
+
+// facts can be defined using dynamic paramter resolved at runtime.
+
+let facts = { a: i, b : 6 , c : i}
+// engine is an asynchronous process that uses promise mechanism
+engine
+  .run(facts).then(events => { // run() returns events with truthy conditions
+  events.map(event => {console.log("returned value " + JSON.stringify(event)); 
+  return k+=event.params.k;})
+  })
+  
+  
   //.then(events => { // run() returns events with truthy conditions
    // events.map(event => {console.log("returned value " + event.type + " k " ); 
     //return k+=event.params.k;})
  // })
-
+/*
 		tmp.then(function (fulfilled) {
     // yay, you got a new phone
     console.log('fullfiled' + JSON.stringify(fulfilled));
@@ -101,17 +149,8 @@ for (i=0;i<9;i++)
 .catch(function (error) {
     // ops, mom don't buy it
     console.log(error.message);
-})
+})*/
   
-}
 
-console.log("k : " + k)
- /*
-	 * .then(triggeredEvents => { // engine returns a list of events with truthy
-	 * conditions triggeredEvents.map(event =>
-	 * console.log(event.params.data.green)) }) .catch(console.log)
-	 */
-  
-//console.log("k " + k);
 
   
