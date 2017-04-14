f840b4310bbef2bb06b19b19c093ebbb4e9d2d27
rumi <rumi@192.168.0.20>
2017-04-13 23:12:20 -0400

    initial commit

---
+++ MyDroolsHelloWorld/src/main/java/com/sample/MyDroolsStateFull.java
@@ -0,0 +1,49 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+public class MyDroolsStateFull {
+
+	public static final void main(String[] args) {
+		try {
+			KieServices ks = KieServices.Factory.get();
+			KieContainer kContainer = ks.getKieClasspathContainer();
+			KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+			MyFacts facts = new MyFacts();
+			FactHandle handle1;
+			
+			int i;
+			
+			facts.setA(8);
+			facts.setB(9);
+			handle1 = kSession.insert(facts);
+
+			long timeStart = System.currentTimeMillis();
+
+			for (i = 0; i < 100000; i++) {
+								
+				facts.swapAB(); // this will force the rule to alternate.
+				facts.setC(i);
+
+				kSession.update(handle1, facts);
+				kSession.fireAllRules();
+
+				//System.out.printf("*a : %d b : %d k : %d\n", facts.a, facts.b, facts.k);
+			}
+
+			long TimeDuration = System.currentTimeMillis() - timeStart;
+
+			System.out.printf("\nDrools Results Iteration # %d duration %d millis result %d\n", i, TimeDuration,
+					facts.k);
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+
+	}
+
+}
---
+++ MyDroolsHelloWorld/src/main/java/com/sample/MyFacts.java
@@ -0,0 +1,44 @@
+package com.sample;
+
+public  final class MyFacts {
+
+	public long getA() {
+		return a;
+	}
+	public void setA(long a) {
+		this.a = a;
+	}
+	public long getB() {
+		return b;
+	}
+	public void setB(long b) {
+		this.b = b;
+	}
+	public long getC() {
+		return c;
+	}
+	public void setC(long c) {
+		this.c = c;
+	}
+	public long getK() {
+		return k;
+	}
+	public void setK(long k) {
+		this.k = k;
+	}
+	
+	public void swapAB() {
+		long temp;
+		temp = a;
+		a = b;
+		b = temp;
+		
+	}
+	
+	public long a = 10;
+	public long b = 9;
+	public long c;
+	//public int i; 
+	public long k=0;
+
+}
