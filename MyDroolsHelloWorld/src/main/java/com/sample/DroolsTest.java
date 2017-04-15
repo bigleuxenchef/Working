b7db2e3798ea1dbf2a30422e1fee153f52680ddc
rumi <rumi@192.168.0.20>
2017-04-15 13:59:29 -0400

    update program + doc

--- MyDroolsHelloWorld/src/main/java/com/sample/DroolsTest.java
+++ MyDroolsHelloWorld/src/main/java/com/sample/DroolsTest.java
@@ -3,6 +3,7 @@ package com.sample;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
 
 /**
  * This is a sample class to launch a rule.
@@ -20,19 +21,38 @@ public class DroolsTest {
             Message message = new Message();
             message.setMessage("Hello World");
             message.setStatus(Message.HELLO);
-            message.test = 1;
+            message.test = 1; // rules will not be fired if test == 1
             
             kSession.insert(message);
+            System.out.println("**** Fire one : fire cascade ****");
             kSession.fireAllRules();
  
             message = new Message();
             message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
-            message.test = 0;
+            message.setStatus(Message.BONJOUR);
+            message.test = 0; // show that a new rule testing a variable will be fired
             
             kSession.insert(message);
+            System.out.println("**** Fire Two : fire different flavour ****");
             kSession.fireAllRules();
             
+            
+            System.out.println("**** Fire Three : loop : fire once ****");
+
+            message = new Message();
+            message.setX(5);
+            message.test = 1; // rules will not be fired if test == 1
+            message.setStatus(4);
+
+            FactHandle handle1  = kSession.insert(message);
+            kSession.fireAllRules();
+            
+            System.out.println("**** Fire Three : loop : fire twice ****");
+            
+            message.setX(3);
+            kSession.update(handle1, message);
+			kSession.fireAllRules();
+
             
         } catch (Throwable t) {
             t.printStackTrace();
@@ -43,12 +63,13 @@ public class DroolsTest {
 
         public static final int HELLO = 0;
         public static final int GOODBYE = 1;
-        
+        public static final int BONJOUR = 2;
+
         public int test;
         private String message;
 
         private int status;
-
+        private int x = 0;
         public String getMessage() {
             return this.message;
         }
@@ -64,6 +85,14 @@ public class DroolsTest {
         public void setStatus(int status) {
             this.status = status;
         }
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
 
     }
 
--- MyDroolsHelloWorld/src/main/java/com/sample/README.md
+++ MyDroolsHelloWorld/src/main/java/com/sample/README.md
@@ -2,7 +2,7 @@
 
 ## Files to know before running Java Samples
 
-### [kmodule.xml](../../../resources/META-INF/kmodule.xml)
+### kmodule.xml[here](../../../resources/META-INF/kmodule.xml)
 
 this file will setup 2 types of connections to Drools : Stateful and Statless, with the stateless being the default cofiguration if nothing is specified
 
@@ -16,9 +16,28 @@ this file will setup 2 types of connections to Drools : Stateful and Statless, w
 </kmodule>
 
 ```
+### Drools files `drl`
+
+If you have created your project with eclipse, those `drl` files should be in `src/main/resources/rules`
+
 
 
 ## DroolsTest.java
+
+This example shows diffent thing :
+
+- fire drools from a stateful connection
+- fire drools several times without recreating an object for each call
+- Nested rules fired
+- Rule setting condition that fires another rule
+- Rule implementing a count down loop
+
+[java program here](DroolsTest.java)
+
+[Drools rules here](../../../resources/rules/Sample.drl)
+
+
+
 
 ## MyDroolsStateFull.java
 
